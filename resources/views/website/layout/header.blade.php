266d7bc407d05da9f1a44192e24095300f54d79c
Aviithapa <abhishekthapa115@gmail.com>
2024-07-08 22:18:33 +0545

    Header icon

--- resources/views/website/layout/header.blade.php
+++ resources/views/website/layout/header.blade.php
@@ -55,7 +55,7 @@
               <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
                   <i class="fa fa-bars"></i>
               </button>
-              <a class="navbar-brand" href="index.html">
+              <a class="navbar-brand" href="{{url('/')}}">
                   <img src="{{ asset('Innov8itcode/assets/img/logo.png') }}" class="logo" alt="Logo">
               </a>
           </div>

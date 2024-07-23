6776511f00cd20c60f470b201a5ae321c1af476b
Aviithapa <abhishekthapa115@gmail.com>
2024-07-23 16:30:47 +0545

    script styling

--- resources/views/website/layout/script.blade.php
+++ resources/views/website/layout/script.blade.php
@@ -1,8 +1,5 @@
 
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
-
-  <script src="{{ asset('Innov8itcode/assets/js/jquery-1.12.4.min.js') }}"></script>
+    <script src="{{ asset('Innov8itcode/assets/js/jquery-1.12.4.min.js') }}"></script>
     <script src="{{ asset('Innov8itcode/assets/js/popper.min.js') }}"></script>
     <script src="{{ asset('Innov8itcode/assets/js/bootstrap.min.js') }}"></script>
     <script src="{{ asset('Innov8itcode/assets/js/jquery.appear.js') }}"></script>
@@ -15,8 +12,8 @@
     <script src="{{ asset('Innov8itcode/assets/js/isotope.pkgd.min.js') }}"></script>
     <script src="{{ asset('Innov8itcode/assets/js/imagesloaded.pkgd.min.js') }}"></script>
     <script src="{{ asset('Innov8itcode/assets/js/count-to.js') }}"></script>
-    <script src="{{ asset('Innov8itcode/assets/js/YTPlayer.min.js') }}"></script>
+    <script src="{{ asset('Innov8itcode/assets/js/YTPlayer.min.js') }}"></script>  
     <script src="{{ asset('Innov8itcode/assets/js/jquery.nice-select.min.js') }}"></script>
-    <script src="{{ asset('Innov8itcode/assets/js/loopcounter.js') }}"></script>
+    <script src="{{ asset('Innov8itcode/assets/js/loopcounter.js') }}"></script> 
     <script src="{{ asset('Innov8itcode/assets/js/bootsnav.js') }}"></script>
     <script src="{{ asset('Innov8itcode/assets/js/main.js') }}"></script>
